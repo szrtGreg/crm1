79cb1d174b0472c287936251fc77799004fdb897
Grzegorz Mutkowski <grzegorz.mutkowski@gmail.com>
2022-04-12 12:55:05 +0200

    2:49

--- leads/urls.py
+++ leads/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import lead_list, lead_detail
+from .views import lead_list, lead_detail, lead_create
 
 
 urlpatterns = [
     path('', lead_list),
-    path('<pk>/', lead_detail),
+    path('create/', lead_create),
+    path('<int:pk>/', lead_detail),
 
 ]
