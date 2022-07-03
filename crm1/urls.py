1597ce414a9174e07f60df350779d9d02d0a30be
Grzegorz Mutkowski <grzegorz.mutkowski@gmail.com>
2022-07-03 20:29:13 +0200

    4:05

--- crm1/urls.py
+++ crm1/urls.py
@@ -13,12 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import LandingPageView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('leads/', include('leads.urls')),
     path('', LandingPageView.as_view(), name='landing-page'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
