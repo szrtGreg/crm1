7304f2fe8e7a77eacd1ceabf033a6a5af92cc67b
Grzegorz Mutkowski <grzegorz.mutkowski@gmail.com>
2022-07-03 20:05:46 +0200

    3:54

--- crm1/urls.py
+++ crm1/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from leads.views import LandingPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('leads/', include('leads.urls')),
+    path('', LandingPageView.as_view(), name='landing-page'),
 ]
