beb9b53a28c3a9d2a803e71c4d06c520c0d7ee5e
Grzegorz Mutkowski <grzegorz.mutkowski@gmail.com>
2022-07-04 11:57:57 +0200

    5:03

--- leads/models.py
+++ leads/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.db.models.signals import post_save
 # Create your models here.
 
 
@@ -18,7 +18,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.email
+        return self.user.username
 
 
 class Lead(models.Model):
@@ -30,3 +30,11 @@ class Lead(models.Model):
 
     def __str__(self):
         return  f'{self.first_name} {self.last_name}'
+
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+    print(instance, created)
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(post_user_created_signal, sender=User)
